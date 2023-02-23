ac471356665e7ccd22b4c672208d6827dd72681e
Brendan Murty <brendan@murty.au>
2023-02-23 13:41:36 +1100

    Bug: Only show cat breed external links if there is one

--- client/src/components/CatBreedDetails/CatBreedDetails.js
+++ client/src/components/CatBreedDetails/CatBreedDetails.js
@@ -26,18 +26,26 @@ class CatBreedDetails extends Component {
           <dd>{this.props.catbreed.temperament}</dd>
           <dt>Origin</dt>
           <dd>{this.props.catbreed.origin}</dd>
+          {this.props.catbreed.vcahospitals_url ? (
+          <>
           <dt>VCA link</dt>
           <dd>
             <a target="_blank" rel="noreferrer" href={this.props.catbreed.vcahospitals_url}>
               VCA Animal Hospitals - {this.props.catbreed.name}
             </a>
           </dd>
+          </>
+          ) : null}
+          { this.props.catbreed.vetstreet_url ? (
+          <>
           <dt>Vetstreet link</dt>
           <dd>
             <a target="_blank" rel="noreferrer" href={this.props.catbreed.vetstreet_url}>
               Vetstreet - {this.props.catbreed.name}
             </a>
           </dd>
+          </>
+          ) : null }
         </dl>
       </section>
     );
